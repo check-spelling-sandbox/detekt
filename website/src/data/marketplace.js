93ff2968d0cc94a2a3c618a4b44fca70bfdad433
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-02-24 07:53:21 -0500

    spelling: operator
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- website/src/data/marketplace.js
+++ website/src/data/marketplace.js
@@ -231,7 +231,7 @@ export const extensions = [
   },
   {
     title: "UseInvokeForOperator",
-    description: "This set contains rules that help to improve readability and to keep a single project-wide convention. UseInvokeForOperater rule detects cases when using invoke expression instead of a direct call with round brackets. The Invoke operator makes code more readable and the rule helps keeping it.",
+    description: "This set contains rules that help to improve readability and to keep a single project-wide convention. UseInvokeForOperator rule detects cases when using invoke expression instead of a direct call with round brackets. The Invoke operator makes code more readable and the rule helps keeping it.",
     repo: "https://github.com/Kiolk/Detekt-rules",
     ruleset: "kiolk-detekt-rules",
     rules: [
